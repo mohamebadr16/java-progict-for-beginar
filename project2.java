a75b07b7b26044997006a3a077a62f4c7afaf183
mohamebadr16 <111384422+mohamebadr16@users.noreply.github.com>
2026-02-06 01:40:17 +0200

    Update project2.java
    ubdate in code

--- project2.java
+++ project2.java
@@ -6,31 +6,28 @@ public class Main{
     Scanner x=new Scanner(System.in);
     
     
-    System.out.printf("Hi\nEnter Your Name ");
+    System.out.printf("Hi\nEnter Your Name: ");
     String name =x.nextLine();
     
     
-    System.out.printf("Enter Your Age ");
+    System.out.printf("Enter Your Age: ");
     int age=x.nextInt();
     
     
-    System.out.printf("Enter Your Weight Of KG ");
+    System.out.printf("Enter Your Weight Of KG: ");
     double weight=x.nextDouble();
     
     
-    System.out.printf("Enter Your Length Of M ");
+    System.out.printf("Enter Your Length Of M: ");
     double length=x.nextDouble();
     
     
     double bmi=weight/(length*length);
     
     
-    System.out.printf("Name is : %s and Age %d%n", name,age);
-    
-    System.out.printf("L %.2f, W %.2f%n", length,weight);
+    System.out.printf("Name: %s%nAge %d years%nWeight: %.2f kg%nLength: %.2f m%nBMI: %.2f ", name,age,weight,length,bmi);
     
     
-    System.out.printf("BMI IS %.2f", bmi);
     
     }
     
